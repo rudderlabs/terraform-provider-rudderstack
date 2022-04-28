0679b0cc5c80c28faa164849e921593dc81e6e9c
Fotis Xenikoudakis <fotis@rudderstack.com>
2022-04-28 10:18:56 +0300

    feat: redshift destination support

---
+++ rudderstack/integrations/destinations/destination_redshift.go
@@ -0,0 +1,124 @@
+package destinations
+
+import (
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
+	c "github.com/rudderlabs/terraform-provider-rudderstack/rudderstack/configs"
+)
+
+func init() {
+	c.Destinations.Register("redshift", c.ConfigMeta{
+		APIType: "RS",
+		Properties: []c.ConfigProperty{
+			c.Simple("host", "host"),
+			c.Simple("port", "port"),
+			c.Simple("database", "database"),
+			c.Simple("user", "user"),
+			c.Simple("password", "password"),
+			c.Simple("namespace", "namespace", c.SkipZeroValue),
+			c.Simple("enableSSE", "enable_sse", c.SkipZeroValue),
+			c.Simple("useRudderStorage", "use_rudder_storage"),
+			c.Simple("syncFrequency", "sync.0.frequency"),
+			c.Simple("syncStartAt", "sync.0.start_at", c.SkipZeroValue),
+			c.Simple("excludeWindow.excludeWindowStartTime", "sync.0.exclude_window_start_time", c.SkipZeroValue),
+			c.Simple("excludeWindow.excludeWindowEndTime", "sync.0.exclude_window_end_time", c.SkipZeroValue),
+			c.Simple("bucketName", "s3.0.bucket_name"),
+			c.Simple("accessKeyID", "s3.0.access_key_id"),
+			c.Simple("accessKey", "s3.0.access_key"),
+		},
+		ConfigSchema: map[string]*schema.Schema{
+			"host": {
+				Type:             schema.TypeString,
+				Required:         true,
+				ValidateDiagFunc: c.StringMatchesRegexp("(^env[.].+)|^(.{1,255})$"),
+			},
+			"port": {
+				Type:             schema.TypeString,
+				Required:         true,
+				ValidateDiagFunc: c.StringMatchesRegexp("(^env[.].+)|^(.{1,100})$"),
+			},
+			"user": {
+				Type:             schema.TypeString,
+				Required:         true,
+				ValidateDiagFunc: c.StringMatchesRegexp("(^env[.].+)|^(.{1,100})$"),
+			},
+			"password": {
+				Type:             schema.TypeString,
+				Required:         true,
+				ValidateDiagFunc: c.StringMatchesRegexp("(^env[.].+)|^(.{1,100})$"),
+				Sensitive:        true,
+			},
+			"database": {
+				Type:             schema.TypeString,
+				Required:         true,
+				ValidateDiagFunc: c.StringMatchesRegexp("(^env[.].+)|^(.{1,100})$"),
+			},
+			"namespace": {
+				Type:     schema.TypeString,
+				Required: true,
+				// ValidateDiagFunc: c.StringMatchesRegexp("(^env[.].*)|^(?!pg_|PG_|pG_|Pg_).*"),
+			},
+			"enable_sse": {
+				Type:     schema.TypeBool,
+				Optional: true,
+			},
+			"use_rudder_storage": {
+				Type:     schema.TypeBool,
+				Required: true,
+			},
+			"sync": {
+				Type:     schema.TypeList,
+				MinItems: 1, MaxItems: 1,
+				Required: true,
+				Elem: &schema.Resource{
+					Schema: map[string]*schema.Schema{
+						"frequency": {
+							Type:             schema.TypeString,
+							Required:         true,
+							ValidateDiagFunc: c.StringMatchesRegexp("^(30|60|180|360|720|1440)$"),
+						},
+						"start_at": {
+							Type:             schema.TypeString,
+							Optional:         true,
+							ValidateDiagFunc: c.StringMatchesRegexp("^([01][0-9]|2[0-3]):[0-5][0-9]$"),
+						},
+						"exclude_window_start_time": {
+							Type:             schema.TypeString,
+							Optional:         true,
+							ValidateDiagFunc: c.StringMatchesRegexp("^([01][0-9]|2[0-3]):[0-5][0-9]$"),
+						},
+						"exclude_window_end_time": {
+							Type:             schema.TypeString,
+							Optional:         true,
+							ValidateDiagFunc: c.StringMatchesRegexp("^([01][0-9]|2[0-3]):[0-5][0-9]$"),
+						},
+					},
+				},
+			},
+			"s3": {
+				Type:     schema.TypeList,
+				MaxItems: 1,
+				Optional: true,
+				Elem: &schema.Resource{
+					Schema: map[string]*schema.Schema{
+						"bucket_name": {
+							Type:             schema.TypeString,
+							Required:         true,
+							ValidateDiagFunc: c.StringMatchesRegexp("(^env[.].+)|^(.{1,100})$"),
+						},
+						"access_key_id": {
+							Type:             schema.TypeString,
+							Optional:         true,
+							ValidateDiagFunc: c.StringMatchesRegexp("(^env[.].+)|^(.{1,100})$"),
+						},
+						"access_key": {
+							Type:             schema.TypeString,
+							Optional:         true,
+							Sensitive:        true,
+							ValidateDiagFunc: c.StringMatchesRegexp("(^env[.].+)|^(.{1,100})$"),
+						},
+					},
+				},
+			},
+		},
+	})
+}
---
+++ rudderstack/integrations/destinations/destination_redshift_test.go
@@ -0,0 +1,80 @@
+package destinations_test
+
+import (
+	"testing"
+
+	"github.com/rudderlabs/terraform-provider-rudderstack/internal/testutil"
+	c "github.com/rudderlabs/terraform-provider-rudderstack/rudderstack/configs"
+)
+
+func TestDestinationResourceRedshift(t *testing.T) {
+	testutil.AssertDestination(t, "redshift", []c.TestConfig{
+		{
+			TerraformCreate: `
+				host = "example.com"
+				port = "5432"
+				user = "example-user"
+				password = "example-password"
+				namespace = "example-namespace"
+				database = "example-database"
+				use_rudder_storage = true
+
+				sync {
+					frequency = "30"
+				}
+			`,
+			APICreate: `{
+				"host": "example.com",
+				"port": "5432",
+				"user": "example-user",
+				"password": "example-password",
+				"database": "example-database",
+				"namespace": "example-namespace",
+				"useRudderStorage": true,
+				"syncFrequency": "30"
+			}`,
+			TerraformUpdate: `
+				host = "example.com"
+				port = "5432"
+				user = "example-user"
+				password = "example-password"
+				namespace = "example-namespace"
+				enable_sse = true
+				database = "example-database"
+				use_rudder_storage = false
+
+				sync {
+					frequency = "30"
+					start_at  = "10:00"
+					exclude_window_start_time = "11:00"
+					exclude_window_end_time   = "12:00"
+				}
+
+				s3 {
+					bucket_name = "some-bucket-name"
+					access_key_id = "some-access-key-id"
+					access_key = "some-access-key"
+				}
+				`,
+			APIUpdate: `{
+				"host": "example.com",
+				"port": "5432",
+				"user": "example-user",
+				"password": "example-password",
+				"database": "example-database",
+				"namespace": "example-namespace",
+				"enableSSE": true,
+				"useRudderStorage": false,
+				"bucketName": "some-bucket-name",
+				"accessKeyID": "some-access-key-id",
+				"accessKey": "some-access-key",
+				"syncFrequency": "30",
+				"syncStartAt": "10:00",
+				"excludeWindow": {
+					"excludeWindowStartTime": "11:00",
+					"excludeWindowEndTime": "12:00"	
+				}
+			}`,
+		},
+	})
+}
