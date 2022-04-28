e41e80f2e54d15ecc52130e5ab09865e0c8768ba
Fotis Xenikoudakis <fotis@rudderstack.com>
2022-04-28 10:18:56 +0300

    feat: s3_datalake destination support

---
+++ rudderstack/integrations/destinations/destination_s3_datalake.go
@@ -0,0 +1,78 @@
+package destinations
+
+import (
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
+	c "github.com/rudderlabs/terraform-provider-rudderstack/rudderstack/configs"
+)
+
+func init() {
+	c.Destinations.Register("s3_datalake", c.ConfigMeta{
+		APIType: "S3_DATALAKE",
+		Properties: []c.ConfigProperty{
+			c.Simple("bucketName", "bucket_name"),
+			c.Simple("prefix", "prefix", c.SkipZeroValue),
+			c.Simple("accessKeyID", "access_key_id", c.SkipZeroValue),
+			c.Simple("accessKey", "access_key", c.SkipZeroValue),
+			c.Simple("enableSSE", "enable_sse", c.SkipZeroValue),
+			c.Simple("useGlue", "use_glue"),
+			c.Simple("region", "region", c.SkipZeroValue),
+			c.Simple("syncFrequency", "sync.0.frequency"),
+			c.Simple("syncStartAt", "sync.0.start_at", c.SkipZeroValue),
+		},
+		ConfigSchema: map[string]*schema.Schema{
+			"bucket_name": {
+				Type:             schema.TypeString,
+				Required:         true,
+				ValidateDiagFunc: c.StringMatchesRegexp("(^\\{\\{.*\\|\\|(.*)\\}\\}$)|(^env[.].+)|^(.{1,100})$"),
+			},
+			"prefix": {
+				Type:             schema.TypeString,
+				Optional:         true,
+				ValidateDiagFunc: c.StringMatchesRegexp("(^\\{\\{.*\\|\\|(.*)\\}\\}$)|(^env[.].+)|^(.{1,100})$"),
+			},
+			"access_key_id": {
+				Type:             schema.TypeString,
+				Optional:         true,
+				ValidateDiagFunc: c.StringMatchesRegexp("(^\\{\\{.*\\|\\|(.*)\\}\\}$)|(^env[.].+)|^(.{1,100})$"),
+			},
+			"access_key": {
+				Type:             schema.TypeString,
+				Optional:         true,
+				Sensitive:        true,
+				ValidateDiagFunc: c.StringMatchesRegexp("(^\\{\\{.*\\|\\|(.*)\\}\\}$)|(^env[.].+)|^(.{1,100})$"),
+			},
+			"enable_sse": {
+				Type:     schema.TypeBool,
+				Optional: true,
+			},
+			"use_glue": {
+				Type:     schema.TypeBool,
+				Required: true,
+			},
+			"region": {
+				Type:             schema.TypeString,
+				Optional:         true,
+				ValidateDiagFunc: c.StringMatchesRegexp("(^env[.].+)|.+"),
+			},
+			"sync": {
+				Type:     schema.TypeList,
+				MinItems: 1, MaxItems: 1,
+				Required: true,
+				Elem: &schema.Resource{
+					Schema: map[string]*schema.Schema{
+						"frequency": {
+							Type:             schema.TypeString,
+							Required:         true,
+							ValidateDiagFunc: c.StringMatchesRegexp("^(30|60|180|360|720|1440)$"),
+						},
+						"start_at": {
+							Type:             schema.TypeString,
+							Optional:         true,
+							ValidateDiagFunc: c.StringMatchesRegexp("^([01][0-9]|2[0-3]):[0-5][0-9]$"),
+						},
+					},
+				},
+			},
+		},
+	})
+}
