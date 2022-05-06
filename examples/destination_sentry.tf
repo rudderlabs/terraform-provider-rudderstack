a06a72ab87336e36569630a9db137d20f9708ac3
Fotis Xenikoudakis <fotis@rudderstack.com>
2022-05-06 14:50:48 +0300

    feat: sentry destination support

---
+++ examples/destination_sentry.tf
@@ -0,0 +1,28 @@
+resource "rudderstack_destination_sentry" "example" {
+  name = "my-sentry"
+
+  config {
+    dsn = "https://example.slack.com"
+
+    # server_name             = "..."
+    # release                 = "..."
+    # environment             = "..."
+    # custom_version_property = "..."
+    # logger                  = "..."
+    # debug_mode              = false
+
+    # ignore_errors = ["one", "two", "three"]
+    # include_paths = ["one", "two", "three"]
+    # allow_urls    = ["one", "two", "three"]
+    # deny_urls     = ["one", "two", "three"]
+
+    # use_native_sdk {
+    #   web = true
+    # }
+
+    # event_filtering {
+    #   whitelist = ["one", "two", "three"]
+    #   blacklist = ["one", "two", "three"]
+    # }
+  }
+}
