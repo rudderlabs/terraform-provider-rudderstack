7ab7338f37362670099fe3d0b4821bbc1060daf8
Rajesh Neti <rajesh@rudderstack.com>
2022-07-11 19:22:49 +0530

    fix mixpanel config

--- rudderstack/integrations/destinations/destination_mixpanel.go
+++ rudderstack/integrations/destinations/destination_mixpanel.go
@@ -21,182 +21,129 @@ func init() {
 			c.Simple("crossSubdomainCookie", "cross_subdomain_cookie"),
 			c.Simple("persistence", "persistence"),
 			c.Simple("secureCookie", "secure_cookie"),
-			c.ArrayWithObjects("superProperties", "super_properties", map[string]string{
-				"property": "property",
-			}),
-			c.ArrayWithObjects("peopleProperties", "people_properties", map[string]string{
-				"property": "property",
-			}),
-			c.ArrayWithObjects("eventIncrements", "event_increments", map[string]string{
-				"property": "property",
-			}),
-			c.ArrayWithObjects("propIncrements", "prop_increments", map[string]string{
-				"property": "property",
-			}),
-			c.ArrayWithObjects("groupKeySettings", "group_key_settings", map[string]string{
-				"groupKey": "group_key",
-			}),
+			c.ArrayWithStrings("superProperties", "property", "super_properties"),
+			c.ArrayWithStrings("peopleProperties", "property", "people_properties"),
+			c.ArrayWithStrings("eventIncrements", "property", "event_increments"),
+			c.ArrayWithStrings("propIncrements", "property", "prop_increments"),
+			c.ArrayWithStrings("groupKeySettings", "groupKey", "group_key_settings"),
 			c.Simple("useNativeSDK.web", "use_native_sdk.0.web"),
-			c.Discriminator("eventFilteringOption", c.DiscriminatorValues{
-				"event_filtering.0.whitelist": "whitelistedEvents",
-				"event_filtering.0.blacklist": "blacklistedEvents",
-			}),
+			c.ArrayWithStrings("whitelistedEvents", "eventName", "event_filtering.0.whitelist"),
+			c.ArrayWithStrings("blacklistedEvents", "eventName", "event_filtering.0.blacklist"),
 			c.ArrayWithStrings("oneTrustCookieCategories.web", "oneTrustCookieCategory", "onetrust_cookie_categories.0.web"),
 		},
 		ConfigSchema: map[string]*schema.Schema{
 			"token": {
-				Type: schema.TypeString,
-				Required: true,
-				Description: "Mixpanel API Token",
-				Sensitive: true,
+				Type:             schema.TypeString,
+				Required:         true,
+				Description:      "Mixpanel API Token",
+				Sensitive:        true,
 				ValidateDiagFunc: c.StringMatchesRegexp("(^\\{\\{.*\\|\\|(.*)\\}\\}$)|(^env[.].+)|^(.{1,100})$"),
 			},
 			"api_secret": {
-				Type: schema.TypeString,
-				Optional: true,
-				Sensitive: true,
-				Description: " Mixpanel API secret",
+				Type:             schema.TypeString,
+				Optional:         true,
+				Sensitive:        true,
+				Description:      "Mixpanel API secret",
 				ValidateDiagFunc: c.StringMatchesRegexp("(^\\{\\{.*\\|\\|(.*)\\}\\}$)|(^env[.].+)|^(.{0,100})$"),
 			},
 			"data_residency": {
-				Type: schema.TypeString,
-				Optional: true,
-				Description: "Mixpanel Server region either us/eu",	
-				ValidateDiagFunc: c.StringMatchesRegexp("^(us|eu)$"),			
+				Type:             schema.TypeString,
+				Required:         true,
+				Description:      "Mixpanel Server region either us/eu",
+				ValidateDiagFunc: c.StringMatchesRegexp("^(us|eu)$"),
 			},
 			"people": {
-				Type: schema.TypeBool,
-				Optional: true,
+				Type:        schema.TypeBool,
+				Optional:    true,
 				Description: "Boolean flag to send all of your identify calls to Mixpanel's People feature ",
 			},
-			"set_all_traits_by_default":{
-				Type: schema.TypeBool,
-				Optional: true,
+			"set_all_traits_by_default": {
+				Type:        schema.TypeBool,
+				Optional:    true,
 				Description: "While this is checked, our integration automatically sets all traits on identify calls as super properties and people properties if Mixpanel People is checked as well.",
 			},
 			"consolidated_page_calls": {
-				Type: schema.TypeBool,
-				Optional: true,
+				Type:        schema.TypeBool,
+				Optional:    true,
 				Description: "This will track Loaded a Page events to Mixpanel for all page method calls. We enable this by default as it's how Mixpanel suggests sending these calls.",
 			},
 			"track_categorized_pages": {
-				Type: schema.TypeBool,
-				Optional: true,
+				Type:        schema.TypeBool,
+				Optional:    true,
 				Description: "This will track events to Mixpanel for page method calls that have a category associated with them. For example page('Docs', 'Index') would translate to Viewed Docs Index Page.",
 			},
 			"track_named_pages": {
-				Type: schema.TypeBool,
-				Optional: true,
+				Type:        schema.TypeBool,
+				Optional:    true,
 				Description: "This will track events to Mixpanel for page method calls that have a name associated with them. For example page('Signup') would translate to Viewed Signup Page.",
 			},
 			"source_name": {
-				Type: schema.TypeString,
-				Optional: true,
-				Description: "This value, if it's not blank, will be sent as rudderstack_source_name to Mixpanel for every event/page/screen call.",
+				Type:             schema.TypeString,
+				Optional:         true,
+				Description:      "This value, if it's not blank, will be sent as rudderstack_source_name to Mixpanel for every event/page/screen call.",
 				ValidateDiagFunc: c.StringMatchesRegexp("(^\\{\\{.*\\|\\|(.*)\\}\\}$)|(^env[.].+)|^(.{0,100})$"),
 			},
 			"cross_subdomain_cookie": {
-				Type: schema.TypeBool,
-				Optional: true,
+				Type:        schema.TypeBool,
+				Optional:    true,
 				Description: "This will allow the Mixpanel cookie to persist between different pages of your application.",
 			},
 			"persistence": {
-				Type: schema.TypeString,
-				Optional: true,
-				Description: "Choose persistence for Mixpanel SDK. One of none|cookie|localStorage",
+				Type:             schema.TypeString,
+				Required:         true,
+				Description:      "Choose persistence for Mixpanel SDK. One of none|cookie|localStorage",
 				ValidateDiagFunc: c.StringMatchesRegexp("^(none|cookie|localStorage)$"),
-				
 			},
 			"secure_cookie": {
-				Type: schema.TypeBool,
-				Optional: true,
+				Type:        schema.TypeBool,
+				Optional:    true,
 				Description: "This will mark the Mixpanel cookie as secure, meaning it will only be transmitted over https",
 			},
 			"super_properties": {
-				Type: schema.TypeList,
-				Optional: true,
+				Type:        schema.TypeList,
+				Optional:    true,
 				Description: "Property to send as super Properties",
-				ConfigMode: schema.SchemaConfigModeAttr,
-				Elem: &schema.Resource{
-					Schema: map[string]*schema.Schema{
-						"property": {
-							Type: schema.TypeString,
-							Required: true,
-							Description: "",
-							ValidateDiagFunc: c.StringMatchesRegexp("(^\\{\\{.*\\|\\|(.*)\\}\\}$)|(^env[.].+)|^(.{0,100})$"),
-						},
-					},
+				Elem: &schema.Schema{
+					Type: schema.TypeString,
 				},
 			},
 			"people_properties": {
-				Type: schema.TypeList,
-				Optional: true,
-				Description: "",
-				ConfigMode: schema.SchemaConfigModeAttr,
-				Elem: &schema.Resource{
-					Schema: map[string]*schema.Schema{
-						"property": {
-							Type: schema.TypeString,
-							Required: true,
-							Description: "",
-							ValidateDiagFunc: c.StringMatchesRegexp("(^env[.].+)|^(.{0,100})$"),
-						},
-					},
+				Type:        schema.TypeList,
+				Optional:    true,
+				Description: "Traits to set as People Properties",
+				Elem: &schema.Schema{
+					Type: schema.TypeString,
 				},
 			},
 			"event_increments": {
-				Type: schema.TypeList,
-				Optional: true,
-				Description: "",
-				ConfigMode: schema.SchemaConfigModeAttr,
-				Elem: &schema.Resource{
-					Schema: map[string]*schema.Schema{
-						"property": {
-							Type: schema.TypeString,
-							Required: true,
-							Description: "",
-							ValidateDiagFunc: c.StringMatchesRegexp("(^env[.].+)|^(.{0,100})$"),
-						},
-					},
+				Type:        schema.TypeList,
+				Optional:    true,
+				Description: "Events to increment in People",
+				Elem: &schema.Schema{
+					Type: schema.TypeString,
 				},
 			},
 			"prop_increments": {
-				Type: schema.TypeList,
-				Optional: true,
-				Description: "",
-				ConfigMode: schema.SchemaConfigModeAttr,
-				Elem: &schema.Resource{
-					Schema: map[string]*schema.Schema{
-						"property": {
-							Type: schema.TypeString,
-							Required: true,
-							Description: "",
-							ValidateDiagFunc: c.StringMatchesRegexp("(^env[.].+)|^(.{0,100})$"),
-						},
-					},
+				Type:        schema.TypeList,
+				Optional:    true,
+				Description: "Properties to increment in People",
+				Elem: &schema.Schema{
+					Type: schema.TypeString,
 				},
 			},
 			"group_key_settings": {
-				Type: schema.TypeList,
-				Optional: true,
-				Description: "",
-				ConfigMode: schema.SchemaConfigModeAttr,
-				Elem: &schema.Resource{
-					Schema: map[string]*schema.Schema{
-						"group_key": {
-							Type: schema.TypeString,
-							Required: true,
-							Description: "",
-							ValidateDiagFunc: c.StringMatchesRegexp("(^env[.].+)|^(.{0,100})$"),
-						},
-					},
+				Type:        schema.TypeList,
+				Optional:    true,
+				Description: "Group Key",
+				Elem: &schema.Schema{
+					Type: schema.TypeString,
 				},
 			},
 			"use_native_sdk": {
 				Type:        schema.TypeList,
 				MaxItems:    1,
 				Optional:    true,
-				Description: "Enable this setting to send events to Mixpanel via the device mode.",
+				Description: "Enable this setting to send the events via the device mode.",
 				Elem: &schema.Resource{
 					Schema: map[string]*schema.Schema{
 						"web": {
@@ -210,7 +157,7 @@ func init() {
 				Type:        schema.TypeList,
 				MaxItems:    1,
 				Optional:    true,
-				Description: "This option allows you filter the events you want to send to Amplitude.",
+				Description: "With this option, you can determine which events are blocked or allowed to flow through to Sentry.",
 				Elem: &schema.Resource{
 					Schema: map[string]*schema.Schema{
 						"whitelist": {
@@ -251,9 +198,6 @@ func init() {
 					},
 				},
 			},
-
-
-
 		},
 	})
 }
