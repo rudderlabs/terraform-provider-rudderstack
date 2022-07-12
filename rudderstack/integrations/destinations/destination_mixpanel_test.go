574c235dca39bdb63c325a4c5c682d57b45fe645
Rajesh Neti <rajesh@rudderstack.com>
2022-07-12 11:29:08 +0530

    update test for mixpanel

--- rudderstack/integrations/destinations/destination_mixpanel_test.go
+++ rudderstack/integrations/destinations/destination_mixpanel_test.go
@@ -7,36 +7,135 @@ import (
 	c "github.com/rudderlabs/terraform-provider-rudderstack/rudderstack/configs"
 )
 
-func TestDestinationResourceMixPanel(t *testing.T) {
+func TestDestinationResourceMixpanel(t *testing.T) {
 	cmt.AssertDestination(t, "mixpanel", []c.TestConfig{
 		{
 			TerraformCreate: `
-				token = "avasaffav1241"
+				token = "..."
 				data_residency = "us"
 				persistence = "none"
 			`,
 			APICreate: `{
-				"token": "avasaffav1241",
-				"dataResidency": "us",
-				"persistence: "none"
-			}"
-			`,
+				"token": "..."
+				"dataResidency": "us"
+				"persistence": "none"
+			}`,
 			TerraformUpdate: `
-				data_residency = "us"
+				api_secret = "..."
 				people = true
 				set_all_traits_by_default = true
+				consolidated_page_calls = true
+				track_categorized_pages = true
+				track_named_pages = true
+				source_name = "my-mixpanel"
+				cross_subdomain_cookie = true
+				secure_cookie = true
+				super_properties = ["one","two","three"]
+				people_properties = ["one","two","three"]
+				event_increments = ["one","two","three"]
+				prop_increments = ["one","two","three"]
+				group_key_settings = ["one","two","three"]
+				
 				use_native_sdk {
 					web = true
 				}
+		
+				event_filtering {
+					whitelist = ["one", "two", "three"]
+					blacklist = ["one","two","three"]
+				}
+		
+				onetrust_cookie_categories {
+					web = ["one", "two", "three"]
+				}
 			`,
-			APIUpdate: `{
-				"dataResidency": "us",
+			APIUpdate: `
+			{
+				"apiSecret": "...",
 				"people": true,
 				"setAllTraitsByDefault": true,
+				"consolidatedPageCalls": true,
+				"trackCategorizedPages": true,
+				"trackNamedPages": true,
+				"sourceName": true,
+				"crossSubdomainCookie": true,
+				"secureCookie": true,
+				"superProperties": [{
+						"property": "one"
+					},
+					{
+						"property": "two"
+					},
+					{
+						"property": "three"
+					}
+				],
+				"peopleProperties": [{
+						"property": "one"
+					},
+					{
+						"property": "two"
+					},
+					{
+						"property": "three"
+					}
+				],
+				"eventIncrements": [{
+						"property": "one"
+					},
+					{
+						"property": "two"
+					},
+					{
+						"property": "three"
+					}
+				],
+				"propIncrements": [{
+						"property": "one"
+					},
+					{
+						"property": "two"
+					},
+					{
+						"property": "three"
+					}
+				],
+				"groupKeySettings": [{
+						"groupKey": "one"
+					},
+					{
+						"groupKey": "two"
+					},
+					{
+						"groupKey": "three"
+					}
+				],
 				"useNativeSDK": {
-				  "web": true
+					"web": "true"
+				},
+				"whitelistedEvents": [{
+						"eventName": "one"
+					},
+					{
+						"eventName": "two"
+					},
+					{
+						"eventName": "three"
+					}
+				],
+				"oneTrustCookieCategories": {
+					"web": [{
+							"oneTrustCookieCategory": "one"
+						},
+						{
+							"oneTrustCookieCategory": "two"
+						},
+						{
+							"oneTrustCookieCategory": "three"
+						}
+					]
 				}
-			}
+			}			
 			`,
 		},
 	})
