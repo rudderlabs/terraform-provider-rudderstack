4bc657de5f217307291d0adeecebfc83bcf459f6
yashasvibajpai <yashasvi.bajpai1999@gmail.com>
2022-11-17 13:01:02 +0530

    revert to original version, add doc template and doc, fix example schema

--- examples/destination_adobe_analytics.tf
+++ examples/destination_adobe_analytics.tf
@@ -2,11 +2,8 @@ resource "rudderstack_destination_adobe_analytics" "example" {
     name = "my-adobe-analytics"
 
     config {
-        tracking_server_url = ""
-        report_suite_id = ""
-        ss_heartbeat = ""
-        heartbeat_tracking_server_url = ""
-        events_to_types = ""
-        marketing_cloud_org_id = ""
+        tracking_server_url = "http://sampleurl.com"
+        report_suite_id = "id01, id02"
+        heartbeat_tracking_server_url = "http://sampleheartbeaturl.com"
     }
 }
