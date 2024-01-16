ee6d98e265d70dd0cdc7c9d10fbd8f15c24be1d1
AASHISH MALIK <imashishmalik.45@gmail.com>
2024-01-16 11:31:10 +0530

    feat: onboarded qualtrics on tf

---
+++ rudderstack/integrations/destinations/destination_qualtrics_test.go
@@ -0,0 +1,61 @@
+package destinations_test
+
+import (
+	"testing"
+
+	cmt "github.com/rudderlabs/terraform-provider-rudderstack/internal/testutil/cm"
+	c "github.com/rudderlabs/terraform-provider-rudderstack/rudderstack/configs"
+)
+
+func TestDestinationResourceQualtrics(t *testing.T) {
+	cmt.AssertDestination(t, "qualtrics", []c.TestConfig{
+		{
+			TerraformCreate: `
+				project_id = "p-id"
+				brand_id = "b-id"
+				use_native_sdk {
+					web = true
+				}
+			`,
+			APICreate: `{
+				"projectId": "p-id",
+				"brandId": "b-id",
+				"useNativeSDK": {
+					"web": true
+				}
+			}`,
+			TerraformUpdate: `
+				project_id = "p-id"
+				brand_id = "b-id"
+
+				enable_generic_page_title = true
+				use_native_sdk {
+					ios = true
+				}
+           
+				event_filtering {
+					blacklist = [ "one", "two", "three" ]
+				}
+				onetrust_cookie_categories = ["one", "two", "three"]
+
+			`,
+			APIUpdate: `{
+				"projectId": "p-id",
+				"brandId": "b-id",
+				"eventFilteringOption": "blacklistedEvents",
+				"blacklistedEvents": [
+					{"eventName": "one"},
+					{"eventName": "two"},
+					{"eventName": "three"}
+				],
+				"useNativeSDK":{"ios":true},
+				"oneTrustCookieCategories": [
+					{ "oneTrustCookieCategory": "one" },
+					{ "oneTrustCookieCategory": "two" },
+					{ "oneTrustCookieCategory": "three" }
+				],
+				"enableGenericPageTitle":{"web":true}
+			}`,
+		},
+	})
+}
