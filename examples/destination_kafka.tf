5a64fb1a9a8069fe0a4f70be5dd20cea5ee3ca91
Yashasvi Bajpai <33063622+yashasvibajpai@users.noreply.github.com>
2024-01-18 04:30:23 +0530

    fix: improve example tf in kafka dest

--- examples/destination_kafka.tf
+++ examples/destination_kafka.tf
@@ -2,10 +2,21 @@ resource "rudderstack_destination_kafka" "example" {
   name = "my-kafka"
 
   config {
-    host_name = "example.com"
-    port      = "9092"
-    topic     = "example-topic"
-    # sasl_enabled = true
+    host_name       = "example.com"
+    port            = "9092"
+    topic           = "example-topic"
+    ssl_enabled     = true
+    # ca_certificate  = "asd"
+    # use_sasl        = true
+    # sasl_type       = "plain"
+    # username        = "foo"
+    # password        = "Rudder123"
+    # convert_to_avro = true
+    # # avro_schema = [{
+    # #   schema_id = "foo",
+    # #   schema    = "bar"
+    # # }]
+    # embed_avro_schema_id       = true
     # onetrust_cookie_categories = ["analytics"]
   }
 
