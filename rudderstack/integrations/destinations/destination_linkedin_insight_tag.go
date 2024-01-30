64ffc8ffe21d4af972c7c5912a892094f03cc70b
AASHISH MALIK <imashishmalik.45@gmail.com>
2024-01-30 20:19:55 +0530

    feat: onboard linkedin insight tag (#71)
    * feat: Linkedin Insight Tag tf
    
    * fix: updated docs

---
+++ rudderstack/integrations/destinations/destination_linkedin_insight_tag.go
@@ -0,0 +1,104 @@
+package destinations
+
+import (
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
+	c "github.com/rudderlabs/terraform-provider-rudderstack/rudderstack/configs"
+)
+
+func init() {
+	c.Destinations.Register("LINKEDIN_INSIGHT_TAG", c.ConfigMeta{
+		APIType: "LINKEDIN_INSIGHT_TAG",
+		Properties: []c.ConfigProperty{
+			c.Simple("partnerId", "partner_id"),
+			c.ArrayWithStrings("whitelistedEvents", "eventName", "event_filtering.0.whitelist"),
+			c.ArrayWithStrings("blacklistedEvents", "eventName", "event_filtering.0.blacklist"),
+			c.Discriminator("eventFilteringOption", c.DiscriminatorValues{
+				"event_filtering.0.whitelist": "whitelistedEvents",
+				"event_filtering.0.blacklist": "blacklistedEvents",
+			}),
+			c.ArrayWithObjects("eventToConversionIdMap", "event_to_conversion_id_map", map[string]string{
+				"from": "from",
+				"to":   "to",
+			}),
+			c.Simple("useNativeSDK.web", "use_native_sdk.0.web"),
+			c.ArrayWithStrings("oneTrustCookieCategories", "oneTrustCookieCategory", "onetrust_cookie_categories"),
+		},
+		ConfigSchema: map[string]*schema.Schema{
+			"partner_id": {
+				Type:        schema.TypeString,
+				Required:    true,
+				Description: "Enter your Partner ID.",
+			},
+			"event_to_conversion_id_map": {
+				Type:        schema.TypeList,
+				Optional:    true,
+				Description: "Event Conversion IDs.",
+				ConfigMode:  schema.SchemaConfigModeAttr,
+				Elem: &schema.Resource{
+					Schema: map[string]*schema.Schema{
+						"from": {
+							Type:        schema.TypeString,
+							Required:    true,
+							Description: "Provide the event name.",
+						},
+						"to": {
+							Type:        schema.TypeString,
+							Required:    true,
+							Description: "Enter the conversion id.",
+						},
+					},
+				},
+			},
+			"event_filtering": {
+				Type:        schema.TypeList,
+				MaxItems:    1,
+				Optional:    true,
+				Description: "With this option, you can determine which events are blocked or allowed to flow through to Google Ads.",
+				Elem: &schema.Resource{
+					Schema: map[string]*schema.Schema{
+						"whitelist": {
+							Type:         schema.TypeList,
+							Optional:     true,
+							Description:  "Enter the event names to be whitelisted.",
+							ExactlyOneOf: []string{"config.0.event_filtering.0.whitelist", "config.0.event_filtering.0.blacklist"},
+							Elem: &schema.Schema{
+								Type: schema.TypeString,
+							},
+						},
+						"blacklist": {
+							Type:         schema.TypeList,
+							Optional:     true,
+							Description:  "Enter the event names to be blacklisted.",
+							ExactlyOneOf: []string{"config.0.event_filtering.0.whitelist", "config.0.event_filtering.0.blacklist"},
+							Elem: &schema.Schema{
+								Type: schema.TypeString,
+							},
+						},
+					},
+				},
+			},
+			"use_native_sdk": {
+				Type:        schema.TypeList,
+				MaxItems:    1,
+				Optional:    true,
+				Description: "As this is a device mode destination, this setting will always be enabled.",
+				Elem: &schema.Resource{
+					Schema: map[string]*schema.Schema{
+						"web": {
+							Type:     schema.TypeBool,
+							Optional: true,
+						},
+					},
+				},
+			},
+			"onetrust_cookie_categories": {
+				Type:        schema.TypeList,
+				Optional:    true,
+				Description: "Specify the OneTrust category name for mapping the OneTrust consent settings to RudderStack's consent purposes.",
+				Elem: &schema.Schema{
+					Type: schema.TypeString,
+				},
+			},
+		},
+	})
+}
