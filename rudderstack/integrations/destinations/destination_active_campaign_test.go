98b3dae611bb3f93a798d2dd325c69264bee16c8
Sai Kumar Battinoju <saibattinoju@rudderstack.com>
2024-01-31 18:34:51 +0530

    fix: test data for consent management

--- rudderstack/integrations/destinations/destination_active_campaign_test.go
+++ rudderstack/integrations/destinations/destination_active_campaign_test.go
@@ -37,16 +37,70 @@ func TestDestinationResourceActiveCampaign(t *testing.T) {
 					shopify = ["one", "two", "three"]
 				}
 				consent_management {
-					web {
-						provider = "provider"
-						resolution_strategy = "resolution_strategy"
-						consents = ["one", "two", "three"]
-					}
-					android {
-						provider = "provider"
-						resolution_strategy = "resolution_strategy"
-						consents = ["one", "two", "three"]
-					}
+					web = [
+						{
+							provider = "oneTrust"
+							consents = ["one_web", "two_web", "three_web"]
+						},
+						{
+							provider = "ketch"
+							consents = ["one_web", "two_web", "three_web"]
+						},
+						{
+							provider = "custom"
+							resolution_strategy = "and"
+							consents = ["one_web", "two_web", "three_web"]
+						}
+					]
+					android = [{
+						provider = "ketch"
+						consents = ["one_android", "two_android", "three_android"]
+					}]
+					ios = [{
+						provider = "custom"
+						resolution_strategy = "and"
+						consents = ["one_ios", "two_ios", "three_ios"]
+					}]
+					unity = [{
+						provider = "custom"
+						resolution_strategy = "or"
+						consents = ["one_unity", "two_unity", "three_unity"]
+					}]
+					reactnative = [{
+						provider = "custom"
+						resolution_strategy = "and"
+						consents = ["one_reactnative", "two_reactnative", "three_reactnative"]
+					}]
+					flutter = [{
+						provider = "custom"
+						resolution_strategy = "and"
+						consents = ["one_flutter", "two_flutter", "three_flutter"]
+					}]
+					cordova = [{
+						provider = "custom"
+						resolution_strategy = "and"
+						consents = ["one_cordova", "two_cordova", "three_cordova"]
+					}]
+					amp = [{
+						provider = "custom"
+						resolution_strategy = "and"
+						consents = ["one_amp", "two_amp", "three_amp"]
+					}]
+					cloud = [{
+						provider = "custom"
+						resolution_strategy = "and"
+						consents = ["one_cloud", "two_cloud", "three_cloud"]
+					}]
+					warehouse = [{
+						provider = "custom"
+						resolution_strategy = "and"
+						consents = ["one_warehouse", "two_warehouse", "three_warehouse"]
+					}]
+					shopify = [{
+						provider = "custom"
+						resolution_strategy = "and"
+						consents = ["one_shopify", "two_shopify", "three_shopify"]
+					}]
 				}
 			`,
 			APIUpdate: `{
@@ -114,34 +168,214 @@ func TestDestinationResourceActiveCampaign(t *testing.T) {
 				"consentManagement": {
 					"web": [
 						{
-							"provider": "provider",
-							"resolutionStrategy": "resolution_strategy",
+							"provider": "oneTrust",
 							"consents": [
 								{
-									"consent": "one"
+									"consent": "one_web"
 								},
 								{
-									"consent": "two"
+									"consent": "two_web"
 								},
 								{
-									"consent": "three"
+									"consent": "three_web"
+								}
+							]
+						},
+						{
+							"provider": "ketch",
+							"consents": [
+								{
+									"consent": "one_web"
+								},
+								{
+									"consent": "two_web"
+								},
+								{
+									"consent": "three_web"
+								}
+							]
+						},
+						{
+							"provider": "custom",
+							"resolutionStrategy": "and",
+							"consents": [
+								{
+									"consent": "one_web"
+								},
+								{
+									"consent": "two_web"
+								},
+								{
+									"consent": "three_web"
 								}
 							]
 						}
 					],
 					"android": [
 						{
-							"provider": "provider",
-							"resolutionStrategy": "resolution_strategy",
+							"provider": "ketch",
 							"consents": [
 								{
-									"consent": "one"
+									"consent": "one_android"
 								},
 								{
-									"consent": "two"
+									"consent": "two_android"
 								},
 								{
-									"consent": "three"
+									"consent": "three_android"
+								}
+							]
+						}
+					],
+					"ios": [
+						{
+							"provider": "custom",
+							"resolutionStrategy": "and",
+							"consents": [
+								{
+									"consent": "one_ios"
+								},
+								{
+									"consent": "two_ios"
+								},
+								{
+									"consent": "three_ios"
+								}
+							]
+						}
+					],
+					"unity": [
+						{
+							"provider": "custom",
+							"resolutionStrategy": "or",
+							"consents": [
+								{
+									"consent": "one_unity"
+								},
+								{
+									"consent": "two_unity"
+								},
+								{
+									"consent": "three_unity"
+								}
+							]
+						}
+					],
+					"reactnative": [
+						{
+							"provider": "custom",
+							"resolutionStrategy": "and",
+							"consents": [
+								{
+									"consent": "one_reactnative"
+								},
+								{
+									"consent": "two_reactnative"
+								},
+								{
+									"consent": "three_reactnative"
+								}
+							]
+						}
+					],
+					"flutter": [
+						{
+							"provider": "custom",
+							"resolutionStrategy": "and",
+							"consents": [
+								{
+									"consent": "one_flutter"
+								},
+								{
+									"consent": "two_flutter"
+								},
+								{
+									"consent": "three_flutter"
+								}
+							]
+						}
+					],
+					"cordova": [
+						{
+							"provider": "custom",
+							"resolutionStrategy": "and",
+							"consents": [
+								{
+									"consent": "one_cordova"
+								},
+								{
+									"consent": "two_cordova"
+								},
+								{
+									"consent": "three_cordova"
+								}
+							]
+						}
+					],
+					"amp": [
+						{
+							"provider": "custom",
+							"resolutionStrategy": "and",
+							"consents": [
+								{
+									"consent": "one_amp"
+								},
+								{
+									"consent": "two_amp"
+								},
+								{
+									"consent": "three_amp"
+								}
+							]
+						}
+					],
+					"cloud": [
+						{
+							"provider": "custom",
+							"resolutionStrategy": "and",
+							"consents": [
+								{
+									"consent": "one_cloud"
+								},
+								{
+									"consent": "two_cloud"
+								},
+								{
+									"consent": "three_cloud"
+								}
+							]
+						}
+					],
+					"warehouse": [
+						{
+							"provider": "custom",
+							"resolutionStrategy": "and",
+							"consents": [
+								{
+									"consent": "one_warehouse"
+								},
+								{
+									"consent": "two_warehouse"
+								},
+								{
+									"consent": "three_warehouse"
+								}
+							]
+						}
+					],
+					"shopify": [
+						{
+							"provider": "custom",
+							"resolutionStrategy": "and",
+							"consents": [
+								{
+									"consent": "one_shopify"
+								},
+								{
+									"consent": "two_shopify"
+								},
+								{
+									"consent": "three_shopify"
 								}
 							]
 						}
