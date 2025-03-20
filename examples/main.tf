0358566d4e0f74791ee974f424db1e33c843e703
Manish Kumar <manish.kumar@rudderstack.com>
2025-03-20 11:35:13 +0530

    feat: update main.tf version to 3.0.2

--- examples/main.tf
+++ examples/main.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     rudderstack = {
       source  = "rudderlabs/rudderstack"
-      version = "~> 3.0.0"
+      version = "~> 3.0.2"
     }
   }
   required_version = "~> 1.10.5"
