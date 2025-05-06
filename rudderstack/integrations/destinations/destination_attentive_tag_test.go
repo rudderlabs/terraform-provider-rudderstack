e7b4672c1899af9a615ce08a8b51078cb1b8e293
Yashasvi Bajpai <33063622+yashasvibajpai@users.noreply.github.com>
2025-05-06 11:01:09 +0530

    feat: onboard attentive tag destination (#139)

---
+++ rudderstack/integrations/destinations/destination_attentive_tag_test.go
@@ -0,0 +1,29 @@
+package destinations_test
+
+import (
+	"testing"
+
+	cmt "github.com/rudderlabs/terraform-provider-rudderstack/internal/testutil/cm"
+	c "github.com/rudderlabs/terraform-provider-rudderstack/rudderstack/configs"
+)
+
+func TestDestinationResourceAttentiveTag(t *testing.T) {
+	cmt.AssertDestination(t, "attentive_tag", []c.TestConfig{
+		{
+			TerraformCreate: `
+				api_key = "key"
+			`,
+			APICreate: `{
+				"apiKey": "key"
+			}`,
+			TerraformUpdate: `
+				api_key = "key"
+				sign_up_source_id = "123456"
+			`,
+			APIUpdate: `{
+				"apiKey": "key",
+				"signUpSourceId": "123456"
+			}`,
+		},
+	})
+}
