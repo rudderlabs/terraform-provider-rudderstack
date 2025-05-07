d06c90cf47382606b1a3d94ec1b9f18934c8a086
Yashasvi Bajpai <33063622+yashasvibajpai@users.noreply.github.com>
2025-05-07 16:40:52 +0530

    chore: bump to version 3.0.5 (#140)

--- examples/main.tf
+++ examples/main.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     rudderstack = {
       source  = "rudderlabs/rudderstack"
-      version = "~> 3.0.4"
+      version = "~> 3.0.5"
     }
   }
   required_version = "~> 1.10.5"
