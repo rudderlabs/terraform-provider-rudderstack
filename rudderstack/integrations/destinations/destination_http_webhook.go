3c70bcde11268fa17e453b1a172cf0ec6425a76c
deepshekhardas <deepshekharadas1234@gmail.com>
2026-03-20 10:02:35 +0530

    feat: add support for HTTP Webhook destination

---
+++ rudderstack/integrations/destinations/destination_http_webhook.go
@@ -0,0 +1,66 @@
+package destinations
+
+import (
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
+
+	c "github.com/rudderlabs/terraform-provider-rudderstack/rudderstack/configs"
+)
+
+func init() {
+	supportedSourceTypes := []string{"web", "android", "ios", "unity", "reactnative", "flutter", "cordova", "amp", "cloud", "warehouse", "shopify"}
+	commonProperties, commonSchema := GetCommonConfigMeta(supportedSourceTypes)
+
+	properties := []c.ConfigProperty{
+		c.Simple("webhookUrl", "webhook_url"),
+		c.Simple("webhookMethod", "webhook_method", c.SkipZeroValue),
+		c.Simple("headers", "headers", c.SkipZeroValue),
+	}
+
+	properties = append(properties, commonProperties...)
+
+	schema := map[string]*schema.Schema{
+		"webhook_url": {
+			Type:             schema.TypeString,
+			Required:         true,
+			Description:      "Enter the HTTP endpoint where RudderStack will send the events.",
+			ValidateDiagFunc: c.StringMatchesRegexp("(^\\{\\{.*\\|\\|(.*)\\}\\}$)|(^env[.].+)|^(?:http(s)?:\\/\\/)?[\\w.-]+(?:\\.[\\w\\.-]+)+[\\w\\-\\._~:/?#[\\]@!\\$&'\\(\\)\\*\\+,;=.]+$"),
+		},
+		"webhook_method": {
+			Type:             schema.TypeString,
+			Optional:         true,
+			Description:      "The HTTP method of the request sent to the configured endpoint. By default, POST is used.",
+			ValidateDiagFunc: c.StringMatchesRegexp("(^\\{\\{.*\\|\\|(.*)\\}\\}$)|(^env[.].+)|(^(POST|PUT|GET)$)"),
+		},
+		"headers": {
+			Type:        schema.TypeList,
+			Optional:    true,
+			Sensitive:   true,
+			Description: "Add custom headers for your events. These headers will be added to the request made from RudderStack to your HTTP webhook endpoint.",
+			ConfigMode:  schema.SchemaConfigModeAttr,
+			Elem: &schema.Resource{
+				Schema: map[string]*schema.Schema{
+					"from": {
+						Type:             schema.TypeString,
+						Required:         true,
+						ValidateDiagFunc: c.StringMatchesRegexp("(^\\{\\{.*\\|\\|(.*)\\}\\}$)|(^env[.].+)|^(.{0,100})$"),
+					},
+					"to": {
+						Type:             schema.TypeString,
+						Required:         true,
+						ValidateDiagFunc: c.StringMatchesRegexp("(^\\{\\{.*\\|\\|(.*)\\}\\}$)|(^env[.].+)|^(.{0,500})$"),
+					},
+				},
+			},
+		},
+	}
+
+	for key, value := range commonSchema {
+		schema[key] = value
+	}
+
+	c.Destinations.Register("http_webhook", c.ConfigMeta{
+		APIType:      "HTTP_WEBHOOK",
+		Properties:   properties,
+		ConfigSchema: schema,
+	})
+}
