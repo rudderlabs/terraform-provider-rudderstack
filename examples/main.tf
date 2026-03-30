002c9f86931ff887be18b468c5b5cc3108d96774
Sudip Paul <sudip.paul1997@gmail.com>
2026-03-30 15:36:59 +0530

    chore: bump new version 4.3.0
    🔒 Scanned for secrets using gitleaks 8.28.0

--- examples/main.tf
+++ examples/main.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     rudderstack = {
       source  = "rudderlabs/rudderstack"
-      version = "~> 4.2.0"
+      version = "~> 4.3.0"
     }
   }
   required_version = "~> 1.10.5"
