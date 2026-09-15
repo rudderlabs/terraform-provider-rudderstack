db06853fa71d2b9b2a65a0c8a322d09f3b2aa93b
tech-infra-rudderstack <tech-infra+github-signing@rudderstack.com>
2026-09-15 09:59:59 +0000

    test: skip live CRUD for Singular and impact destinations

--- rudderstack/integrations/destinations/destination_singular_test.go
+++ rudderstack/integrations/destinations/destination_singular_test.go
@@ -188,5 +188,8 @@ func TestDestinationResourceSingular(t *testing.T) {
 }
 
 func TestAccDestinationSingular(t *testing.T) {
+	if !acc.PlanOnly() {
+		t.Skip("skipping: API create returns 500 in the shared E2E workspace for this destination")
+	}
 	acc.AccAssertDestination(t, "singular", singularTestConfigs)
 }
